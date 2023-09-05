6353bcc2aeb28616621c603c9301e50536fe4454
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-04 23:52:29 -0300

    feat: implementing payment message on payment method in payment-service

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePaymentDTO } from './create-payment.dto';
 import { PaymentStatus } from '@prisma/client';
+import { lastValueFrom } from 'rxjs';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    @Inject('PAYMENTS_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
 
   all() {
     return this.prismaService.payment.findMany();
@@ -18,6 +24,7 @@ export class PaymentsService {
         status: PaymentStatus.APPROVED,
       },
     });
+    await lastValueFrom(this.kafkaClient.emit('payments', payment));
     return payment;
   }
 }
