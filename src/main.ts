b74236dcc78332bc2e90c3742460388a4b029333
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-04 23:43:45 -0300

    feat: implementing consumer abstraction on main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'payments',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'payments-consumer',
+      },
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(3001);
 }
 bootstrap();
